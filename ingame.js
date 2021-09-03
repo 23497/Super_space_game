21cfdd316b92ef3c3080d2c362bfe375e6c0663d
23497 <79152493+23497@users.noreply.github.com>
2021-09-03 13:18:08 -0500

    Add files via upload

---
+++ ingame.js
@@ -0,0 +1,48 @@
+function Corona()
+{
+    window.location('Corona.html');
+}
+function Polluz()
+{
+    window.location('Polluz.html');
+}
+function pleiades()
+{
+    window.location('Pleiades.html');
+}
+function Orion_belt()
+{
+    window.location('Orion_belt.html');
+}
+function Castor()
+{
+    window.location('Castor.html');
+}
+function Cappella()
+{
+    window.location('Cappella.html');
+}
+function Mankin_nebula()
+{
+    window.location('Mankin_nebula.html');
+}
+function Betelgeuse()
+{
+    window.location('Betelgeuse.html');
+}
+function Bellatrix()
+{
+    window.location('Bellatrix.html');
+}
+function Audi_australi()
+{
+    window.location('Audi_Australi.html');
+}
+function Aldebaran()
+{
+    window.location('Aledbaran.html');
+}
+function Al_nath()
+{
+    window.location('Al_nath.html');
+}
---
+++ main.js
@@ -0,0 +1,9 @@
+function Play()
+{
+    window.location('indep.html');
+}
+
+function M35()
+{
+    window.location('M35.html');
+}
